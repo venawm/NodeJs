e4427a9cb92478c5a0c5f45e14a055305069c7aa
Samir_Pandey <pandesamir3@gmail.com>
2022-06-01 11:18:09 +0545

    v 1.1.0 <added some stylings>

--- Chapter 1/server.js
+++ Chapter 1/server.js
@@ -1,11 +1,13 @@
 const express = require(`express`)
 const http = require('http')
+const path = require('path')
 
 const adminRoutes = require('./routes/admin')
 const userRoutes = require('./routes/shop')
 const app = express()
 const PORT  = 3000
 
+app.use(express.static(path.join(__dirname,'public')))
 app.use(adminRoutes)
 app.use(userRoutes)
 

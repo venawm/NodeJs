5db6d0baeb75be14fb4f403b06bd06ee11c58e42
Samir_Pandey <pandesamir3@gmail.com>
2022-06-01 13:09:28 +0545

    AIP

--- Chapter 1/routes/admin.js
+++ Chapter 1/routes/admin.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const app = express()
+const { appendFile } = require('fs')
 const path = require('path')
+const bodyParser = require('body-parser')
 const router = express.Router()
+const products =[]
+
+app.use(bodyParser.urlencoded({extended:false}))
 
 router.get('/admin/add-product',(req,res,next)=>{
     res.sendFile(path.join(__dirname,'views','add-product.html'))
 })
 
-module.exports = router;
+router.post('/admin/add-product',(req,res,next)=>{
+    
+    products.push({title:req.body.title})
+    res.redirect('/')
+    
+})
+router.get('/admin/add-products/list',(req,res,next)=>{
+    res.send(products)
+})
+exports.routes = router
+exports.products = products
--- Chapter 1/server.js
+++ Chapter 1/server.js
@@ -1,14 +1,15 @@
 const express = require(`express`)
 const http = require('http')
 const path = require('path')
+const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const userRoutes = require('./routes/shop')
 const app = express()
 const PORT  = 3000
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')))
-app.use(adminRoutes)
+app.use(adminData.routes)
 app.use(userRoutes)
 
 const server = http.createServer(app)

107ca6be3094e064410fd8b4224bd856775ae9bc
Samir_Pandey <pandesamir3@gmail.com>
2022-06-01 13:09:10 +0545

    integrated a variables to store all the products and display it using  a api

--- Chapter 1/routes/shop.js
+++ Chapter 1/routes/shop.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
 const router = express.Router()
+const adminData = require('./admin')
 
 router.use('/',(req,res,next)=>{
     res.sendFile(path.join(__dirname,'views','shop.html'))
+    console.log(adminData.products)
    
 })
+
 
 module.exports = router;

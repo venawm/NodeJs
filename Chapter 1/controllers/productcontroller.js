e9843e2c5a75537be33808cfcf314126ae6edf01
Samir_Pandey <pandesamir3@gmail.com>
2022-06-02 19:15:32 +0545

    Added models to complete the model views controller model(MVC)

--- Chapter 1/controllers/productcontroller.js
+++ Chapter 1/controllers/productcontroller.js
@@ -1,13 +1,16 @@
-const products = []
+const Product = require('../model/product')
+
 
 function addProducts(req,res,next){
     console.log(req.body)
-    products.push({title:req.body.title})
+    const product = new Product(req.body.title);
+    product.save()
     res.redirect('/')
     
 }
 function viewProducts(req,res,next){
+    const products = Product.fetchAll()
     res.send(products)
 }
 module.exports = {addProducts,viewProducts}
-exports.products = products
+ 
